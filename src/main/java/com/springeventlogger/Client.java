228e46d7c252e3745e470372bab8fe4deb790f6c
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-10-31 01:01:19 -0400

    init

---
+++ src/main/java/com/springeventlogger/Client.java
@@ -0,0 +1,32 @@
+package com.springeventlogger;
+
+/**
+ * Danylo Sashchuk <p>
+ * 10/31/23
+ */
+
+public class Client {
+    private String id;
+    private String fullName;
+
+    public Client(String id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
---
+++ src/main/java/com/springeventlogger/ConsoleEventLogger.java
@@ -0,0 +1,12 @@
+package com.springeventlogger;
+
+/**
+ * Danylo Sashchuk <p>
+ * 10/31/23
+ */
+
+public class ConsoleEventLogger {
+    public void logEvent(String msg) {
+        System.out.println(msg);
+    }
+}
